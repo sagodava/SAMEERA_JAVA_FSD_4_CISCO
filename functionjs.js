78cedd9965b594407cfdab69d0a4b6a056eaa30a
sagodava <89145430+sagodava@users.noreply.github.com>
2021-10-28 19:28:01 +0530

    Add files via upload

---
+++ functionjs.js
@@ -0,0 +1,11 @@
+// program to print the text
+// declaring a function
+function greet(name) {
+  console.log("Hello " + name + ":)");
+}
+
+// variable name can be different
+let name = "Virendra";
+
+// calling function
+greet(name);
